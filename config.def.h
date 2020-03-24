256a6b1a13551cbe889e054ceb942deb523858b8
Kishan <kishan@kishan.sh>
2020-03-24 22:23:56 +0530

    updating config

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Dank Mono:size=15:antialias=true:autohint=true";
+static char *font = "Dank Mono:size=14:antialias=true:autohint=true";
 static int borderpx = 10;
 
 /*
@@ -25,7 +25,7 @@ char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
-static float chscale = 0.8;
+static float chscale = 1.0;
 
 /*
  * word delimiter string
