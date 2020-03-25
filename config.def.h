831a3c7d159828e652a57dc1ff5f641d130215f2
Kishan <kishan@kishan.sh>
2020-03-26 04:10:31 +0530

    Hack Font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Dank Mono:size=14:antialias=true:autohint=true";
+static char *font = "Hack:size=14:antialias=true:autohint=true";
 static int borderpx = 10;
 
 /*
